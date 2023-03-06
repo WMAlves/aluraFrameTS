65dce9551b810e698dd69bc2178b19a364d47948
WMAlves <williamma03@gmail.com>
2023-03-06 14:08:36 -0300

    alterado o estilo de criação do constructor, onde fica mais simples a criação

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,11 @@
 export class Negociacao{
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
+ 
 
-    constructor(data: Date, quantidade: number, valor: number){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date, 
+        private _quantidade: number, 
+        private _valor: number
+        ) {}
 
     get data():Date{
         return this._data;
@@ -16,6 +14,8 @@ export class Negociacao{
     get quantidade():number{
         return this._quantidade;
     }
+
+    
 
     
     get valor():number{
