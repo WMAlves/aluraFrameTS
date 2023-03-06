3676dac3637b6190aa14d12f1e8ad6c77409b198
WMAlves <williamma03@gmail.com>
2023-03-06 16:59:49 -0300

    Nova maneira de declaração de array com generics, utilizado o Readonly Array, utilizaod o modificador readonly, getters vs propriedades públicas em modo de leitura, programçaão defensiva.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,12 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = [];
+    private negociacoes: Array<Negociacao> = []; //informa array da classe negociação, outra forma de fazer isso é: "private negociacoes: Negociacao[]"
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao> {
+    lista(): ReadonlyArray<Negociacao> { //para deixar o array apenas para leitura, outra forma de fazer isso seria, "lista(): readonly Negociacao[]"
         return this.negociacoes;
         
     }
