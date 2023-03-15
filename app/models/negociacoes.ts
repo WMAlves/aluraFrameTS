92161e78b3918a16c7c658886c73fabd950dfcf4
WMAlves <williamma03@gmail.com>
2023-03-15 11:20:55 -0300

    foram implementados o conceitos de classes e de orientação objeto, criando classes abstratas e que utilizam o herança.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,7 +3,7 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = []; //informa array da classe negociação, outra forma de fazer isso é: "private negociacoes: Negociacao[]"
     adiciona(negociacao: Negociacao) {
-        this.negociacoes.push(negociacao);
+        this.negociacoes.push(negociacao);//aqui está adicionando as negociações na lista.
     }
 
     lista(): ReadonlyArray<Negociacao> { //para deixar o array apenas para leitura, outra forma de fazer isso seria, "lista(): readonly Negociacao[]"
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,9 +1,9 @@
 export class Negociacoes {
     constructor() {
-        this.negociacoes = [];
+        this.negociacoes = []; //informa array da classe negociação, outra forma de fazer isso é: "private negociacoes: Negociacao[]"
     }
     adiciona(negociacao) {
-        this.negociacoes.push(negociacao);
+        this.negociacoes.push(negociacao); //aqui está adicionando as negociações na lista.
     }
     lista() {
         return this.negociacoes;
