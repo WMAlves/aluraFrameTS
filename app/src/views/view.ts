ee5f0db6c6718485a4a21bf013baabb219e70c9d
WMAlves <williamma03@gmail.com>
2023-03-21 13:29:33 -0300

    nesse ponto foi adicionado os Decorator como parâmetros, foi criado decorator de inspesão e substituido a função protect pelo dacorator escape, para evitar ataques.

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,33 +1,26 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T>{
 
     protected elemento: HTMLElement;
-    private escapar = false;
 
-    constructor(seletor: string, escapar?:boolean) {
+
+    constructor(seletor: string) {
         const elemento = document.querySelector(seletor);
-        if(elemento){
+        if (elemento) {
             this.elemento = elemento as HTMLElement;
         }
-        else{
+        else {
             throw Error(`Seletor ${seletor} não existe no DOM, verifique.`)
         }
-        
-        if (escapar){
-            this.escapar=escapar; 
-        }
-    }
-    @logarTempoDeExecucao()
-    public update(model: T): void {
-        
-        let template = this.template(model);
-        if(this.escapar){
-            template=template.replace(/<script>[\s\S]*?<\/script>/,'');
-        }
-        this.elemento.innerHTML = template;
-       
-    }
 
+    }
+    @inspect()
+    @logarTempoDeExecucao(true)
+    public update(model: T): void {
+        let template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
     protected abstract template(model: T): string
 }
